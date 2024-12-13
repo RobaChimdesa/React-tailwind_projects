8a0aeabb018c5ddf251e773463cefe3edb0e02fe
RobaChimdesa <robachimdesa123@gmail.com>
2024-12-13 23:46:39 +0300

    Initial commit

--- coffe_web/coffe_web/src/components/pageForAll.jsx
+++ coffe_web/coffe_web/src/components/pageForAll.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import NavbarForAllUser from './NavbarForAllUser'
-import About from './About'
-import Home from './Home'
-import Footer from './Footer'
+import React from "react";
+import NavbarForAllUser from "./NavbarForAllUser";
+import About from "./About";
+import Home from "./Home";
+import Footer from "./Footer";
 const PageForAll = () => {
   return (
     <div>
-      <NavbarForAllUser/>
-      <Home/>
-      <About/>
-      <Footer/>
-    </div>
-  )
-}
+      <NavbarForAllUser />
+      <div id="home">
+        <Home />
+      </div>
 
-export default PageForAll
+      <div id="about">
+        <About />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default PageForAll;
